2a1f6fc5231502fa847859166baa39604b77dd64
Shidhartha Sen <shidhartha98619@gmail.com>
2026-01-12 16:31:49 -0600

    added all folders and files needed to set up the frontend and backend

---
+++ backend/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.api.v1.routes import health
+
+app = FastAPI(
+    title="Project API",
+    version="1.0.0"
+)
+
+app.include_router(
+    health.router,
+    prefix="/api/v1",
+    tags=["health"]
+)
+
