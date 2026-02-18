9ab79c86cb38cb0c14525f16e401d8b2ac1dbf7d
Muazzam Arif <sgd408@usask.ca>
2026-02-18 14:49:55 -0600

    database.sql- created the listing images table for the database.

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
   items_sold integer NOT NULL DEFAULT 0,
   CONSTRAINT users_email_unique UNIQUE (email)
 );
+
 CREATE TABLE listings (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   seller_id uuid NOT NULL,
@@ -19,3 +20,13 @@ CREATE TABLE listings (
     FOREIGN KEY (seller_id) REFERENCES users(user_id)
     ON DELETE NO ACTION ON UPDATE NO ACTION
 );
+
+CREATE TABLE listing_images (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  listing_id uuid NOT NULL,
+  image_url text NOT NULL,
+  CONSTRAINT fk_listing_images_listing
+    FOREIGN KEY (listing_id) REFERENCES listings(id)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
